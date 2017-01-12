388b38c4eb3310ef0673223ce650db82f0b68ade
Joshua Richardson <joshua@richardson.tech>
2017-01-12 00:57:52 +0000

    End of Chapter 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import getpass
+
+
+context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!", 'username': getpass.getuser()}
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says this is the about page <br /><a href='/rango/'>Home</a>")
+    return render(request, 'rango/about.html', context=context_dict)
