a93dd6c44196031171603037e505e61e48a74677
Joshua Richardson <joshua@richardson.tech>
2017-01-13 11:53:23 +0000

    Finishes Ch10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -14,12 +14,11 @@ from rango.models import Category, Page
 
 
 def index(request):
+    visitor_cookie_handler(request)
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': pages_list}
-    response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
-    return response
+    context_dict = {'categories': category_list, 'pages': pages_list, 'visits': request.session['visits']}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
@@ -138,15 +137,22 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
-def visitor_cookie_handler(request, response):
-    visits_cookie = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-    visits = 0
     if (datetime.now() - last_visit_time).days > 0:
-        visits = visits_cookie + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -128,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
